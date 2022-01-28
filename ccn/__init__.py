1bb85cbe769e1a3f2dc0689271427eae585ef54e
Alex Tatomir <atatomir5@gmail.com>
2022-01-28 01:29:53 +0000

    Move test/train/draw into ccn

--- ccn/__init__.py
+++ ccn/__init__.py
@@ -3,3 +3,4 @@ from .constraint import Constraint
 from .constraints_group import ConstraintsGroup
 from .constraints_module import ConstraintsModule
 from .constraints_layer import ConstraintsLayer
+from .util import train, test, draw_classes
---
+++ ccn/util.py
@@ -0,0 +1,52 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+def train(dataloader, model, clayer, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):   
+        # Compute prediction error
+        pred = model(X)
+        constrained = clayer(pred, goal=y)
+        loss = loss_fn(constrained, y)
+        
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, clayer, loss_fn):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss = 0
+    correct = 0.
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            pred = clayer(pred)
+            test_loss += loss_fn(pred, y).item()
+            correct += (torch.where(pred > 0.5, 1., 0.) == y).sum(dim=0)
+    test_loss /= size
+    correct /= size
+
+    accuracy = ", ".join([f"{100 * rate:>0.1f}%" for rate in correct])
+    print(f"Test Error: \n Accuracy: {accuracy}%")
+    print(f" Avg loss: {test_loss:>8f} \n")
+
+def draw_classes(model, draw = None):
+  dots = np.arange(0., 1., 0.01, dtype = "float32")
+  grid = torch.tensor([(x, y) for x in dots for y in dots])
+  preds = model(grid).detach()
+
+  classes = preds.shape[1]
+  fig, ax = plt.subplots(1, classes)
+  for i, ax in enumerate(ax):
+    image = preds[:, i].view((len(dots), len(dots)))
+    if draw != None: draw(ax, i)    
+    ax.imshow(image, cmap='hot', interpolation='nearest', origin='lower', extent=(0., 1., 0., 1.))
+  plt.show()
